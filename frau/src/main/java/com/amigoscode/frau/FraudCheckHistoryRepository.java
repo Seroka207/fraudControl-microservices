d6fbfc2db57160af974e2cdc5bd36f351b6866d1
Seroka207 <seroka207@gmail.com>
2022-02-21 23:40:08 +0300

    first commit

---
+++ frau/src/main/java/com/amigoscode/frau/FraudCheckHistoryRepository.java
@@ -0,0 +1,6 @@
+package com.amigoscode.frau;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory,Integer> {
+}
---
+++ frau/src/main/java/com/amigoscode/frau/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.amigoscode.frau;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
